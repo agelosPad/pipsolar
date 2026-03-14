057d9c81b3bcda928471a2ffc3d504f350fbbdf0
agelosPad <159722760+agelosPad@users.noreply.github.com>
2026-03-14 11:43:11 +0200

    Update __init__.py

--- components/pipsolar/select/__init__.py
+++ components/pipsolar/select/__init__.py
@@ -16,7 +16,7 @@ CONF_CHARGER_SOURCE_PRIORITY = "charger_source_priority"
 CONF_CHARGING_DISCHARGING_CONTROL = "charging_discharging_control"
 CONF_CURRENT_MAX_CHARGING_CURRENT = "current_max_charging_current"
 CONF_CURRENT_MAX_AC_CHARGING_CURRENT = "current_max_ac_charging_current"
-####
+CONF_BATTERY_TYPE = "battery_type"
 
 
 PipsolarSelect = pipsolar_ns.class_("PipsolarSelect", cg.Component, select.Select)
