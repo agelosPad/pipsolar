358a43cb49cde238bb8051fccb126964d97d9a7e
agelosPad <159722760+agelosPad@users.noreply.github.com>
2026-03-14 11:58:04 +0200

    Update pipsolar.cpp

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -110,10 +110,10 @@ void Pipsolar::loop() {
         //  this->battery_type_->publish_state(value_battery_type_);
         //}
         //  select for battery_type
-        //if (this->battery_type_select_) {
-        //  std::string value = esphome::to_string(value_battery_type_);
-        //  this->battery_type_select_->map_and_publish(value);
-        //}
+        if (this->battery_type_select_) {
+          std::string value = esphome::to_string(value_battery_type_);
+          this->battery_type_select_->map_and_publish(value);
+        }
         if (this->current_max_ac_charging_current_) {
           this->current_max_ac_charging_current_->publish_state(value_current_max_ac_charging_current_);
         }
